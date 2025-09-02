16c4887dc65effcd67c4e9bb4c72cdc234c1651c
Jesus David Olmos <jolmos@solucionesamericana.com.co>
2025-09-02 14:40:38 -0500

    Updates

--- assets/BlankLayout-8dhIuwpe.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as s}from"./index-COLQ4D2l.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx(s,{})});export{e as default};
---
+++ assets/BlankLayout-Ce_g3JWC.js
@@ -0,0 +1,1 @@
+import{j as t,O as s}from"./index-CYQDOk0P.js";const e=()=>t.jsx(t.Fragment,{children:t.jsx(s,{})});export{e as default};
